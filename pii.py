bad8e2748448708e02f04c6050535ebde59c5116
Pierre Lalanne <pierre.lalanne@gitguardian.com>
2026-06-10 17:51:31 +0200

    Update pii.py

--- pii.py
+++ pii.py
@@ -1,1 +1,12 @@
 Employee SSN: 234-56-7890 on record.
+
+Please wire to IBAN AT61 1904 3002 3457 3201
+
+Some more PII:
+
+Charged the credit card 4539-1488-0343-6467 successfully.
+
+Employee A SIN: 046-454-285
+Employee B SIN: 046-454-286
+Employee C SIN: 046-454-287
+Employee D SIN: 046-454-288
