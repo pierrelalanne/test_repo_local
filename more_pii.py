01010858b20d868b9f312a27c45ae1c41df7150c
Pierre Lalanne <pierre.lalanne@gitguardian.com>
2026-07-31 17:18:49 +0200

    Update more_pii.py

--- more_pii.py
+++ more_pii.py
@@ -1,10 +1,8 @@
-Employee SSN: 234-56-7890 on record.
+No more PII here.
 
-Please wire to IBAN AT61 1904 3002 3457 3201
 
 Some more PII:
 
-Charged the credit card 4539-1488-0343-6467 successfully.
 
   
 
